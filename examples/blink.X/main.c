3600e05877772a85746bbc50548ff5fc3996e958
Paul Konstantin Gerke <paulkgerke@craftware.info>
2018-08-19 23:40:39 +0200

    Add blink example

---
+++ examples/blink.X/main.c
@@ -0,0 +1,54 @@
+// PIC16LF1705 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = ON        // Internal/External Switchover Mode (Internal/External Switchover Mode is enabled)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is enabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PPS1WAY = ON     // Peripheral Pin Select one-way control (The PPSLOCK bit cannot be cleared once it is set by software)
+#pragma config ZCDDIS = ON      // Zero-cross detect disable (Zero-cross detect circuit is disabled at POR)
+#pragma config PLLEN = ON       // Phase Lock Loop enable (4x PLL is always enabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+
+#include <xc.h>
+
+#define _XTAL_FREQ 8000000
+#include <freq.h>
+#include <timer0.h>
+
+void interrupt int_handler() {
+    timer0_ih(NULL);
+}
+
+int main() {
+    OSCCON = OSCCON_BITS;
+    
+    timer0_init(NULL);
+    GIE = 1;
+
+    // Setup the timer library...
+    TRISCbits.TRISC0 = 0;
+    LATCbits.LATC0 = 0;
+    while (1) {
+        GIE = 0;
+        if (pic170x_timer0.ms > 1000) {
+            LATCbits.LATC0 = !LATCbits.LATC0;
+            pic170x_timer0.ms -= (uint32_t) 1000;
+        }
+        GIE = 1;
+    }
+}
