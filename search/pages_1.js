71790224bcfe77ea5929e079c8175618343e7b9f
Paul Konstantin Gerke <paulkgerke@craftware.info>
2018-08-21 00:36:56 +0200

    Add documentation for v0.1

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['libpic170x_20library',['libpic170x library',['../index.html',1,'']]],
-  ['license',['LICENSE',['../md_LICENSE.html',1,'']]]
+  ['license',['LICENSE',['../md_LICENSE.html',1,'']]],
+  ['libpic170x_20library_20_2d_20v0_2e1',['libpic170x library - v0.1',['../md_README.html',1,'']]]
 ];
---
+++ v0.1/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ms',['ms',['../structTimer0.html#a5fe5c367c4c2bcf13ab57c78a121f321',1,'Timer0']]]
+];
